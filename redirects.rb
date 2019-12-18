64def3655f197ff26dd109c7261a39cabffe3cb4
Melinda Gutermuth <mgutermuth@pivotal.io>
2019-12-18 10:26:31 -0500

    Remove non-working redirects [#169906379]

--- redirects.rb
+++ redirects.rb
@@ -2,7 +2,3 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
-
-r301 %r{/platform/2-8/stemcells/(?![\d-]+)(.*)}, "/platform/stemcells/$1"
-r301 %r{/platform/2-7/stemcells/(?![\d-]+)(.*)}, "/platform/stemcells/$1"
-r301 %r{/pivotalcf/2-6/stemcells/(?![\d-]+)(.*)}, "/platform/stemcells/$1"
