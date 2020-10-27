0cc303e8114141055b9dd6c3c85faa2ab4f6ac93
Severine Tymon <s.tymon@gmail.com>
2020-10-27 13:54:22 -0700

    [#174604562] add a redirect to remove version #s

--- redirects.rb
+++ redirects.rb
@@ -2,3 +2,5 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
+
+r301 %r{/stemcells/(2-[3-9]|2-\d\d+)/(.*)}, '/stemcells/$2'
