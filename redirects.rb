9dd462c17f31d8c565d42ae6b4d8c6fb6545f4e1
Samia Nneji <snneji@vmware.com>
2023-02-08 13:08:25 +0000

    Add redirects

--- redirects.rb
+++ redirects.rb
@@ -4,3 +4,8 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 r301 %r{/stemcells/(2-[3-9]|2-\d\d+)/(.*)}, '/stemcells/$2'
+
+# Redirect from docs.pivotal.io/stemcells to docs.vmware.com Stemcell release notes docs
+r301  %r{/stemcells/([\w-]*\.html)}, "https://docs.vmware.com/en/Stemcells-for-VMware-Tanzu/services/release-notes/$1"
+r301  %r{/stemcells/}, "https://docs.vmware.com/en/Stemcells-for-VMware-Tanzu/services/release-notes/index.html"
+r301  %r{/stemcells}, "https://docs.vmware.com/en/Stemcells-for-VMware-Tanzu/services/release-notes/index.html"
